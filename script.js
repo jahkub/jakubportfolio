4c9a4cc22b3ad97ba29cce0d7f3f94bfafa4e20f
jakub-bembenek-nti-johanneberg <jakub.bembenek@elev.ga.ntig.se>
2023-03-11 14:50:26 +0100

    fixat navbaren
    Nu fungerar den så som jag vill funktionsmässigt. Olika på dator och mobil. Den är inte klar när det gäller utseende men det fixar jag i framtiden.

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-window.onscroll = function() {invisNav()};
-
-
-function invisNav() {
-    if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-        navbar.classList.add("visable")
-    } else {
-        navbar.classList.remove("visable");
-        }
-
-};
 let navbar = document.querySelector(".navbar");
 let burger = document.querySelector(".burger")
+let navToggle = document.querySelector('.navToggle');
+let lines = document.querySelectorAll('.line');
 
-burger.addEventListener("click", burgerOpen);
+
+navToggle.addEventListener("click", burgerOpen);
 
 function burgerOpen() {
     console.log("burger toggled " + burger);
     navbar.classList.toggle("open")
+    burger.classList.toggle('circle')
+        for (let i = 0; i < lines.length; i++) { 
+            lines[i].classList.toggle('crisscross')
+        }
+
+}
+
+
+
+
+window.onscroll = function() {invisNav(width)}
+
+
+var width = window.matchMedia("(min-width: 768px)")
+function invisNav(width) {
+    if (width.matches) {
+        if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) { {
+        navbar.classList.add("visable")
+            console.log('we adding')
+    } } 
+    else {
+        navbar.classList.remove("visable");
+            console.log('we removing')
+    }
+    }   
+
+    if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+    for (let i = 0; i < lines.length; i++) { 
+        lines[i].classList.add('lineOpacity')
+    }     }
+    else {
+    for (let i = 0; i < lines.length; i++) { 
+        lines[i].classList.remove('lineOpacity')
+    }
+    }
 }
